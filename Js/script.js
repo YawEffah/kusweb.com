db8cb1449828c9f0467298732fef57ae9442bb48
YawEffah <effahofficial99@gmail.com>
2024-02-24 12:49:08 +0000

    First commit

---
+++ Js/script.js
@@ -0,0 +1,37 @@
+	function opennav() {
+		document.getElementById("Nav").style.width="100%";
+	}
+
+	function closenav() {
+		document.getElementById("Nav").style.width='0';
+	}
+
+	function opensearch() {
+		document.getElementById("search-form").style.width="100%";
+	}
+
+	function closesearch() {
+		document.getElementById("search-form").style.width="100px";
+	}
+
+
+	function openmsearch() {
+		document.getElementById("m-search-form").style.opacity="0.9";
+		document.getElementById("search-icon").style.display="none";
+	}
+
+	function closemsearch() {
+		document.getElementById("search-icon").style.display="block";
+		document.getElementById("m-search-form").style.opacity="0";
+	}
+
+
+	function openlogin() {
+		document.getElementById("Login-form").style.width="320px";
+	}
+
+	function closelogin() {
+		document.getElementById("Login-form").style.width="0";
+	}
+
+	
